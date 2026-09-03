8679c42b1710cf10bde1d4f92416581c8bf1f5a8
AntoriaDEV <mitchell.christopher@aol.com>
2026-09-03 14:31:31 -0500

    Updated

--- Assets/_RailShooter/RingScript.cs
+++ Assets/_RailShooter/RingScript.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class RingScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public GameObject ringMesh;
+    public Vector3 rotateSpeed;
+
+    private void Update()
     {
-        
+        ringMesh.transform.Rotate(rotateSpeed * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
